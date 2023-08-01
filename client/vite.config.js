09ae933b9fb4469fd2feb137edc0df55950c4dd7
Kirin28 <kirin.kulikova@gmail.com>
2023-08-01 10:11:05 +0200

    disabling externalizing 2.0

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,22 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-  proxy: {
-  "/api": {
-  target: "http://localhost:4000",
-  changeOrigin: true,
-  secure: false,
+    proxy: {
+      "/api": {
+        target: "http://localhost:4000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   build: {
     rollupOptions: {
-      external: []
-    }
-  }
+      external: [],
+    },
   },
-  },
-  },
-  },
-  )
+});
