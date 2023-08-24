23b4ae4da7decd8f92dfce2c28cedce7c4b71af8
Kirin28 <kirin.kulikova@gmail.com>
2023-08-24 11:59:04 +0200

    added analytics tool

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -8,19 +8,19 @@ export default function Footer() {
       <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
    
     <div className="me-5 d-none d-lg-block">
-      <span className="fw-semibold">Get connected with me!</span>
+      <span className="fw-semibold">Get connected with us!</span>
     </div>
     <div>
-      <a href="https://www.facebook.com/profile.php?id=100005787222294" target="_blank" rel="noreferrer" className="me-4 text-reset">
+      <a href="#" className="me-4 text-reset">
         <i className="fab fa-facebook-f"></i>
       </a>
-      <a href="https://www.linkedin.com/in/irene-kulikova-763bb7229/" target="_blank" rel="noreferrer" className="me-4 text-reset">
+      <a href="#" target="_blank" rel="noreferrer" className="me-4 text-reset">
         <i className="fab fa-linkedin"></i>
       </a>
-      <a href="https://t.me/KittenTailss" target="_blank" rel="noreferrer" className="me-4 text-reset">
+      <a href="#" target="_blank" rel="noreferrer" className="me-4 text-reset">
         <i className="fab fa-telegram"></i>
       </a>
-      <a href="https://github.com/Kirin28" target="_blank" rel="noreferrer" className="me-4 text-reset">
+      <a href="#" target="_blank" rel="noreferrer" className="me-4 text-reset">
         <i className="fab fa-github"></i>
       </a>
     </div>
